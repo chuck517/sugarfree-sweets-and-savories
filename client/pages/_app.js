42ae17d1c9e36f0baae5164b075a406121539586
Derek Pizzolatto <derek.pizzolatto@gmail.com>
2021-03-08 08:02:52 -0600

    fix: addToCart and removeFromCart behaving properly

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,38 +1,44 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 
-import { CartContext } from '../contexts/CartContext';
+import { CartContext, addToCart, removeFromCart } from '../contexts/CartContext';
 import '../styles/globals.css';
 
 
 function MyApp({ Component, pageProps }) {
   const [cart, setCart] = useState([]);
-
+  
   const addToCart = (product) => {
-    if (!cart.includes(product)) {
+    const foundItem = cart.find(item => item.id === product.id);
+    if (!foundItem) {
       product.quantity = 1;
       setCart([...cart, product]);
-    } else {      
-      const index = cart.indexOf(product);
-      const [ itemToAdd ] = cart.filter((item) => item.id === product.id);
-      itemToAdd.quantity++;
+    } else {
+      const index = cart.indexOf(foundItem);
+      foundItem.quantity++;
       const updatedCart = cart;
-      updatedCart[index] = itemToAdd;
+      updatedCart[index] = foundItem;
       setCart(updatedCart);
     }
   }  
   
   const removeFromCart = (product) => {
-    if (cart.includes(product)) {
-      if (product.quantity === 1) {
-        const updatedCart = cart.filter((item) => product.name !== item.name)
+    const foundItem = cart.find(item => item.id === product.id);
+    if (foundItem) {
+      if (foundItem.quantity === 1) {
+        const updatedCart = cart.filter(item => product.name !== item.name)
         setCart(updatedCart);
-      } else if (product.quantity > 1) {
-        product.quantity--;
+      } else {
+        const index = cart.indexOf(foundItem);
+        foundItem.quantity--;
+        const updatedCart = cart;
+        updatedCart[index] = foundItem;
+        setCart(updatedCart);
       }
     }
   }
 
   const inventory = useMemo(() => ({ cart, setCart }), [cart, setCart]);
+  
   return (
     <CartContext.Provider value={inventory}>
       <Component {...pageProps}
